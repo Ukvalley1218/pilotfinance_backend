9b28e164eb1b60b045e902833869b301cc524eb0
tejas kahirnar <tejaskhairnar256@gmail.com>
2026-01-24 13:02:33 +0530

    commit changes in server .js

--- server.js
+++ server.js
@@ -119,6 +119,8 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
   "https://pf.valleyhoster.com",
+  "https://pfrequrt.valleyhoster.com/",
+  "https://pf.valleyhoster.com/",
 ];
 
 app.use(
