f308fa2dcfcd40391534c64450f7d18302cf63d0
tejas kahirnar <tejaskhairnar256@gmail.com>
2026-02-10 17:29:32 +0530

    save commit

--- src/routes/admin/partnerRoutes.js
+++ src/routes/admin/partnerRoutes.js
@@ -17,7 +17,7 @@ router.get("/", getAllPartners);
 router.post("/", createPartner);
 router.get("/:id", getPartnerById);
 router.put("/:id", updatePartner);
-router.put("/:id", verifyPartnerKYC);
+router.put("/verify/:id", verifyPartnerKYC);
 router.delete("/:id", deletePartner);
 
 export default router;
